ee0affc152884a60b416a93af71c2ed60bb1664f
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-08-18 22:51:07 +0900

    docs : solved test17~19.py

---
+++ 과제/calc.py
@@ -0,0 +1,15 @@
+def sum(a, b):
+    return f"{a} + {b} = {a+b}"
+
+
+def sub(a, b):
+    return f"{a} - {b} = {a-b}"
+
+
+def multi(a, b):
+    return f"{a} * {b} = {a*b}"
+
+
+def div(a, b):
+    return f"{a} / {b} = {a/b}"
+
