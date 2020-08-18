cfc168fb427f4e6950f41d9877498165eba57592
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-08-18 11:13:38 +0900

    docs : upload files

---
+++ 과제/test24.py
@@ -0,0 +1,10 @@
+"""
+[문제] 아래 country 리스트를 오름차순정렬하시오
+country = ["Korea", "japan", "CHINA", "america"]
+
+[출력 화면]
+['CHINA', 'Korea', 'america', 'japan'] ==>대소문자 구별하여 오름차순 정렬
+['america', 'CHINA', 'japan', 'Korea'] ==>대소문자 구별하지 않고 정렬
+"""
+#----------------------------------------------------------
+country = ["Korea", "japan", "CHINA", "america"]
---
+++ 과제/test37.py
@@ -0,0 +1,11 @@
+'''
+[문제] 리스트 내포 기능을 이용해 다음 문장으로부터 모음('aeiou')을 제거하십시오.
+'Python is powerful... and fast; plays well with others; runs everywhere; is friendly & easy to learn; is Open.'
+       조건) 문자를 연결 할때 join()을 이용하시오
+
+[출력 화면]
+Pythn s pwrfl... nd fst; plys wll wth thrs; rns vrywhr; s frndly & sy t lrn; s Opn.
+'''
+#--------------------------------------------------------------
+data_str = 'Python is powerful... and fast; plays well with others; runs everywhere; is friendly & easy to learn; is Open.'
+serch = 'aeiou'
